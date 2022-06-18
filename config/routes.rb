5804016d1b0638be0edc7318d549999b3d958a70
Corey Ellis <coreypozzatiellis@gmail.com>
2022-06-18 17:09:55 +1000

    revised routes to include only the actions necessary

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,7 @@
 Rails.application.routes.draw do
-  get 'users/new'
-  get 'users/create'
-  get 'users/update'
-  get 'users/edit'
-  get 'users/destroy'
   devise_for :users
-  resources :lives
   resources :users, except: [:index, :show]
+  resources :lives
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
