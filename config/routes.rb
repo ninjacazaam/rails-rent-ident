19b5cce011c9a5c0ffb73fc4a8eabce46862352b
Jasperzhang <zed0416660907@gmail.com>
2022-06-21 19:51:17 +1000

    bookings

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   resources :users, except: [:index, :show]
   resources :lives
+  resources :bookings
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
