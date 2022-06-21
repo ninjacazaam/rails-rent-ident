d48ceb5b6992b49e1dd4d4a1ee5987fd971629f8
Jasperzhang <zed0416660907@gmail.com>
2022-06-21 20:02:05 +1000

    routes modified

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   resources :users, except: [:index, :show]
-  resources :lives
-  resources :bookings
+  resources :lives, :bookings
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
